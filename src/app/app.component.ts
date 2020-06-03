86ce90d4e9a8baa784a0ea020a501e5d856e4682
sobusz <53800916+sobusz@users.noreply.github.com>
2020-06-03 13:04:44 +0200

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Courses';
   term: string;
+
+  constructor(){
+    document.body.style.background = '#ecf0f1';
+  }
 }
